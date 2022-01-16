f8d2b335a80f368096b76b66473b2fa26b466238
Austin Abro <AustinAbro321@gmail.com>
2022-01-16 12:54:26 -0500

    Use native reporter for whole test session using pytest fixture Co-Authored-By: David Bernstein <tobeagile> Co-Authored-By: 4dsherwood <4dsherwood@gmail.com> Co-Authored-By: Llewellyn Falco <llewellyn.falco@gmail.com> Co-Authored-By: SwamyDev <raml.bernhard@gmail.com>

--- start_point/test_hiker.py
+++ start_point/test_hiker.py
@@ -2,12 +2,17 @@ from hiker import global_answer, Hiker
 from approvaltests.approvals import verify
 from approvaltests.reporters import PythonNativeReporter
 from approvaltests import Options
+from approvaltests import set_default_reporter
+import pytest
 
+@pytest.fixture(autouse=True)
+def run_around_tests():
+    set_default_reporter(PythonNativeReporter())
 
 def test_global():
     result = str(global_answer())
-    verify(result, options=Options().with_reporter(PythonNativeReporter()))
+    verify(result)
 
 def test_instance():
     result = str(Hiker().instance_answer())
-    verify(result, options=Options().with_reporter(PythonNativeReporter()))
+    verify(result)
