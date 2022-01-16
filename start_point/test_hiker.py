8c7d3c61dfe767dc87c7a2657a9e08131c2a98e4
Austin Abro <AustinAbro321@gmail.com>
2022-01-16 12:36:45 -0500

    Changed sha to newer version with approvals  changes Added newlines in approvals files Added specific reporter Co-Authored-By: David Bernstein <tobeagile> Co-Authored-By: 4dsherwood <4dsherwood@gmail.com> Co-Authored-By: Llewellyn Falco <llewellyn.falco@gmail.com> Co-Authored-By: SwamyDev <raml.bernhard@gmail.com>

--- start_point/test_hiker.py
+++ start_point/test_hiker.py
@@ -1,11 +1,13 @@
 from hiker import global_answer, Hiker
 from approvaltests.approvals import verify
+from approvaltests.reporters import PythonNativeReporter
+from approvaltests import Options
 
 
 def test_global():
     result = str(global_answer())
-    verify(result)
+    verify(result, options=Options().with_reporter(PythonNativeReporter()))
 
 def test_instance():
     result = str(Hiker().instance_answer())
-    verify(result)
+    verify(result, options=Options().with_reporter(PythonNativeReporter()))
